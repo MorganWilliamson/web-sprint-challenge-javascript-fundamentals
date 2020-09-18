daef4479b7cd2ac598b9fd8e0d9f7c5de7aa8407
MorganWilliamson <mwilliamson7695@gmail.com>
2020-09-18 11:21:06 -0500

    Working on closure.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,22 @@ myFunction();
 
 // Explanation: 
 
+// The internal variable is accessible because a function can reach one level deeper into itself, and that combined with the inner layer pushing information back up to its parent allows the console.log to work. myFunction is invoking nestedFunction, which is just console.log-ing the internal variable. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function counting(){
+  for(let i = 0; i < 10; i++){
+    return i;
+  };
+};
+
+function summation(num){
+  let counting = [];
+  counting.reduce(function(accumulator, item){
+    return accumulator + item.num;
+  }, 0)
+};
+console.log(summation(4));
